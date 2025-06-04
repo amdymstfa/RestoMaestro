ee0bafa09fcf50051685c16d59d386e792c4e21d
amdymstfa <amdymoustapha011@gmail.com>
2025-06-04 20:18:46 +0100

    Add initial setup: models, controllers, and routes for restaurant system

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\TableController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::resource('users', UserController::class);
+Route::resource('tables', TableController::class);
+

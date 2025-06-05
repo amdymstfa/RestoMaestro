98bb9a5900903d4cf984b232ff8f63d13d1a6021
amdymstfa <amdymoustapha011@gmail.com>
2025-06-06 00:14:30 +0100

    feat: add Menu model with relationships and fillable attributes

--- app/Models/Menu.php
+++ app/Models/Menu.php
@@ -2,15 +2,31 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Menu extends Model
 {
+    use HasFactory;
+
     protected $fillable = [
         'name',
         'description',
+        'type',
         'price',
-        'available',
+        'is_special',
     ];
 
+    protected $casts = [
+        'price' => 'decimal:2',
+        'is_special' => 'boolean',
+    ];
+
+    /**
+     * Get the order items for the menu.
+     */
+    public function orderItems()
+    {
+        return $this->hasMany(OrderItem::class, 'menu_id');
+    }
 }
